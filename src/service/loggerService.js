32c9d192bf3da5b49e6370b3f6339b53ad9b9351
Santosh M <santoshm@santoshs-MacBook-Pro.local>
2020-12-17 12:09:04 +0530

    Feat #DP-1442 feat : Change all logs to follow the standard log format - Sourcing

--- src/service/loggerService.js
+++ src/service/loggerService.js
@@ -7,28 +7,28 @@ const telemetryEventConfig = require('../config/telemetryEventConfig.json');
         "ets": 1518460198146,
         "ver": "3.0",
         "mid": '',
-        "actor": { // required. actor who generated the log.
-          "id": logObject['actorId'], // id of the user, api, or job
-          "type": "API" // type of actor - user, API, Job
+        "actor": { 
+          "id": logObject['actorId'], 
+          "type": "API" 
         },
-        "context": { // required. context in which the log is generated.
-          "channel": logObject['channel'], // name of the service that generated the log
-          "env": logObject['env'], // name of the module or API, e.g.: controller, manager, auth, etc
-          "did": "", // Device Id of the user if available
-          "sid": "", // Corresponding user session ID if available
+        "context": { 
+          "channel": logObject['channel'],
+          "env": logObject['env'], 
+          "did": "", 
+          "sid": "", 
           "pdata": telemetryEventConfig.pdata,
-          cdata: [] // Optional. 
+          cdata: [] 
         },
-        "object": { // Optional
-          "id": "", // The system level object id. For ex: content/collection/group id
-          "type": "" // Type of the object. For ex: Content, Course, Textbook etc
+        "object": { 
+          "id": "", 
+          "type": "" 
         },
         "edata": {
-          "type": "system", // Required. Type of log (system)
-          "level": logObject['level'], // Required. Level of the log. TRACE, DEBUG, INFO, WARN
-          "requestid": '', // Required. Trace id of the request.
-          "message": logObject['msg'], // Required. Log message(errmsg).
-          "params": [logObject.params] // Optional. Additional params in the log message
+          "type": "system", 
+          "level": logObject['level'],
+          "requestid": '', 
+          "message": logObject['msg'], 
+          "params": [logObject.params]
         }
       }
       return logFormate;
