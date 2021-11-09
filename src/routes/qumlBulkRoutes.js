62c9a74e457c3de3a94406a6d8bd8e66be8c7297
Vaibhav Bhuva <vaibhav.bhuva@tarento.com>
2021-11-09 15:00:14 +0530

    Issue #SB-23374 feat : question bulk upload status API issue fixes

--- src/routes/qumlBulkRoutes.js
+++ src/routes/qumlBulkRoutes.js
@@ -4,12 +4,12 @@ const qumlRequestMiddleware = require('../validators/qumlBulkUploadValidators');
 const BASE_URL = '/question/v1';
 
 module.exports = function (app) {
-  app.route(BASE_URL + '/bulkupload/')
+  app.route(BASE_URL + '/bulkupload')
     .post(requestMiddleware.gzipCompression(), requestMiddleware.createAndValidateRequestBody,
     requestMiddleware.checkChannelID,
     qumlRequestMiddleware.qumlBulkUploadValidator(),qumlRequestMiddleware.validate, qumlBulkUpload.bulkUpload);
   
-    app.route(BASE_URL + '/bulkUploadStatus')
+    app.route(BASE_URL + '/bulkupload/status')
       .post(requestMiddleware.gzipCompression(), requestMiddleware.createAndValidateRequestBody,
       qumlBulkUpload.qumlSearch);
   }
