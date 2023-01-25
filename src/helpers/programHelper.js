547c0df9b5ba4657b86bb151109b9b9dfce615f9
vaishali <vaishali_k@techjoomla.com>
2023-01-25 09:41:26 +0530

    Issue #co-267 fix: Request traceability with proper logs

--- src/helpers/programHelper.js
+++ src/helpers/programHelper.js
@@ -1310,56 +1310,6 @@ class ProgramServiceHelper {
       loggerService.exitLog(rspObj.responseCode, logObject);
     }
   }
-
-  loggerError(data,errCode) {
-    var errObj = {}
-    errObj.eid = 'Error'
-    errObj.edata = {
-      err : errCode,
-      errtype : data.errMsg,
-      requestid : data.msgId || uuid(),
-      stacktrace : _.truncate(JSON.stringify(data), { 'length': stackTrace_MaxLimit})
-    }
-    logger.error({msg: 'Error log', errObj})
-  }
-
-  getParams(data, status, errCode, msg) {
-    var params = {}
-    params.resmsgid = data.msgid || uuid()
-    params.msgid = data.msgid || null
-    params.status = status
-    params.err = errCode
-    params.errmsg = msg
-
-    return params
-  }
-
-  successResponse(data) {
-    var response = {}
-    response.id = data.apiId
-    response.ver = data.apiVersion
-    response.ts = new Date()
-    response.params = getParams(data, 'successful', null, null)
-    response.responseCode = data.responseCode || 'OK'
-    response.result = data.result
-    return response
-  }
-
-  /**
-   * function create error response body.
-   * @param {Object} data
-   * @returns {nm$_responseUtil.errorResponse.response}
-   */
-  errorResponse(data,errCode) {
-    var response = {}
-    response.id = data.apiId
-    response.ver = data.apiVersion
-    response.ts = new Date()
-    response.params = this.getParams(data, 'failed', data.errCode, data.errMsg)
-    response.responseCode = errCode ? errCode +'_'+ data.responseCode : data.responseCode
-    response.result = data.result
-    return response
-  }
 }
 
 module.exports = ProgramServiceHelper;
